2289eedc1936cda34542066dfc97913929cb7c7f
Berkan Duymayan <86782845+berkandymn@users.noreply.github.com>
2024-02-16 15:36:55 +0300

    Library sınıfı oluşturuldu
    library sınıfı içerisinde kitap ekleme, listeleme ve silme işlemleri yapıldı.

---
+++ libraryAutomation.py
@@ -0,0 +1,58 @@
+class library:
+    def __init__(self,kayıtlar="books.txt"):
+        self.kayıtlar=open(kayıtlar,"a+")
+    def __del__(self):
+        self.kayıtlar.close()
+        
+    def listBooks(self):
+        with open("books.txt","r") as kitap:
+            booklist=[]
+            for book in kitap.read().splitlines():
+                booklist=book.split(",")
+                kitapAdı=booklist[0]
+                yazarAdı=booklist[1]
+                yayınYılı=booklist[2]
+                syfSayisi=booklist[3]
+                print(f"Kitabın Adı: {kitapAdı} - Yazarı: {yazarAdı} - Yayın Yılı: {yayınYılı} - Sayfa Sayısı: {syfSayisi}")
+        
+    def addBook(self):
+        with open("books.txt","a+") as kitap:
+            kitapAdı=input("Kitabın Adı: ")
+            yazarAdı=input("Yazar Adı: ")
+            yayınYılı=input("Yayın Yılı: ")
+            syfSayisi=input("Sayfa Sayısı: ")
+            kitapBilgisi=(f"{kitapAdı},{yazarAdı},{yayınYılı},{syfSayisi}")
+            kitap.write(str(kitapBilgisi)+"\n")
+
+    def removeBook(self):
+        books=library()
+        books.listBooks()
+        with open("books.txt","r+") as kitap:
+            bookName=input("Silinecek Kitabın ismi: ")
+            booklist=[]
+            kitaplar=[]
+            for book in kitap:
+                booklist=book.strip().split(",")
+                kitapAdı=booklist[0]
+                yazarAdı=booklist[1]
+                yayınYılı=booklist[2]
+                syfSayisi=booklist[3]
+                if kitapAdı==bookName:
+                    bookName=(f"{kitapAdı},{yazarAdı},{yayınYılı},{syfSayisi}")
+                else:
+                    kitaplar.append(book)
+        
+            with open("books.txt","w") as kitap2:
+                for kitap in kitaplar:
+                    kitap2.write(kitap)
+
+
+#%%hs
+
+nesne=library()
+nesne.listBooks()
+
+
+#%%
+nesne=library()
+nesne.removeBook()
