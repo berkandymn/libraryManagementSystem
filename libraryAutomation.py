d03427e7124f4c523d4f8547827ac6b8179bd7fb
Berkan Duymayan <86782845+berkandymn@users.noreply.github.com>
2024-02-18 21:49:42 +0300

    Silme işlemi dönüt düzenlemesi
    silme işleminin yapılıp yapılmadığına dair bir kontrol mekanizması ve geri dönüt mesajı eklendi.

--- libraryAutomation.py
+++ libraryAutomation.py
@@ -1,30 +1,30 @@
 class library:
-    def __init__(self,kayıtlar="books.txt"):
-        self.kayıtlar=open(kayıtlar,"a+")
-    def __del__(self):
-        self.kayıtlar.close()
+    def __init__(self,kayitlar="books.txt"):
+        self.kayitlar=open(kayitlar,"a+",encoding="utf-8")
+        self.kayitlar.close()
         
     def listBooks(self):
         with open("books.txt","r",encoding="utf-8") as kitap:
             booklist=[]
             for book in kitap.read().splitlines():
                 booklist=book.split(",")
-                kitapAdı=booklist[0]
-                yazarAdı=booklist[1]
-                yayınYılı=booklist[2]
+                kitapAdi=booklist[0]
+                yazarAdi=booklist[1]
+                yayinYili=booklist[2]
                 syfSayisi=booklist[3]
-                print(f"Kitap: {kitapAdı} - {yazarAdı} - {yayınYılı} - {syfSayisi}")
+                print(f"Kitap: {kitapAdi} - {yazarAdi} - {yayinYili} - {syfSayisi}")
         
     def addBook(self):
-        with open("books.txt","a+") as kitap:
-            kitapAdı=input("Kitabın Adını Giriniz: ")
-            yazarAdı=input("Yazar Adını Giriniz: ")
-            yayınYılı=input("Yayın Yılını Giriniz: ")
+        with open("books.txt","a+",encoding="utf-8") as kitap:
+            kitapAdi=input("Kitabın Adını Giriniz: ")
+            yazarAdi=input("Yazar Adını Giriniz: ")
+            yayinYili=input("Yayın Yılını Giriniz: ")
             syfSayisi=input("Sayfa Sayısını Giriniz: ")
-            kitapBilgisi=(f"{kitapAdı},{yazarAdı},{yayınYılı},{syfSayisi}")
+            kitapBilgisi=(f"{kitapAdi},{yazarAdi},{yayinYili},{syfSayisi}")
             kitap.write(str(kitapBilgisi)+"\n")
 
     def removeBook(self):
+        silinecek=False
         books=library()
         books.listBooks()
         with open("books.txt","r+",encoding="utf-8") as kitap:
@@ -33,38 +33,33 @@ class library:
             kitaplar=[]
             for book in kitap:
                 booklist=book.strip().split(",")
-                kitapAdı=booklist[0]
-                yazarAdı=booklist[1]
-                yayınYılı=booklist[2]
+                kitapAdi=booklist[0]
+                yazarAdi=booklist[1]
+                yayinYili=booklist[2]
                 syfSayisi=booklist[3]
-                if kitapAdı==bookName:
-                    bookName=(f"{kitapAdı},{yazarAdı},{yayınYılı},{syfSayisi}")
+                if kitapAdi==bookName:
+                    bookName=(f"{kitapAdi},{yazarAdi},{yayinYili},{syfSayisi}")
+                    silinecek=True
                 else:
                     kitaplar.append(book)
-        
+            if silinecek==True:
+                print("\n"+bookName+" Silindi")
+            elif silinecek==False:
+                print("\n"+"Silinecek Kitap Bulunamadı.")
+                        
             with open("books.txt","w",encoding="utf-8") as kitap2:
                 for kitap in kitaplar:
                     kitap2.write(kitap)
 
-"""def menu(islem):
-    if islem==1:
-        lib.listBooks()
-    elif islem==2:
-        lib.addBook()
-    elif islem==3
-        lib.removeBook()
-    elif islem==4:
-        break"""
 lib=library()
 
 while True:
     secenek=input("\n***MENU***\n1 - Kitapları Listele\n2 - Kitap Ekle\n3 - Kitabı Kaldır\n4 - Çıkış\n")
-    match secenek:
-        case "1":
-            lib.listBooks()
-        case "2":
-            lib.addBook()
-        case "3":
-            lib.removeBook()
-        case "4":
-            break
+    if secenek=="1":
+        lib.listBooks()
+    elif secenek=="2":
+        lib.addBook()
+    elif secenek=="3":
+        lib.removeBook()
+    elif secenek=="4":
+        break
